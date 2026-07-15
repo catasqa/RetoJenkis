49e8ace966ff8fd239d54386472377dcb050bc3d
Csilvestreg21 <kattha16@gmail.com>
2026-07-15 14:11:54 -0500

    fix: agregar imports de List, ArrayList y Filter

--- src/test/java/co/sqa/reto/runners/RetoApiRunner.java
+++ src/test/java/co/sqa/reto/runners/RetoApiRunner.java
@@ -5,23 +5,12 @@ import co.com.sqa.reto.tasks.ConsultarUsuarios;
 import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.rest.abilities.CallAnApi;
-import org.junit.jupiter.api.BeforeAll; 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-// === ¡NUEVAS IMPORTACIONES PARA EVITAR EL ERROR! ===
-import java.util.ArrayList;
-import java.util.List;
-import io.restassured.filter.Filter;
-// ===================================================
-
 import static net.serenitybdd.screenplay.GivenWhenThen.*;
 import static org.hamcrest.Matchers.equalTo;
-
-import io.restassured.RestAssured;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 
 @ExtendWith(SerenityJUnit5Extension.class)
 public class RetoApiRunner {
@@ -29,17 +18,9 @@ public class RetoApiRunner {
     private final Actor analistaQA = Actor.named("Analista QA");
     private final String baseUrl = "https://reqres.in";
 
-    @BeforeAll
-    public static void setup() {
-        // Ahora sí compilará perfectamente usando listas mutables
-        List<Filter> filters = new ArrayList<>();
-        filters.add(new RequestLoggingFilter());
-        filters.add(new ResponseLoggingFilter());
-        RestAssured.filters(filters);
-    }
-
     @BeforeEach
     public void configurarActor() {
+        // Serenity se encarga de acoplar RestAssured con el actor de forma interna y segura
         analistaQA.can(CallAnApi.at(baseUrl));
     }
 
